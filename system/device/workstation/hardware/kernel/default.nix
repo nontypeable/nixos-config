e079629fb0e1c8545723f50183a974d36ed576f3
Andrey Sadkov <ad.sadkov@gmail.com>
2025-02-22 23:55:51 +0300

    function (workstation-hardware): added kernel configuration
    - Added kernel configuration for the `workstation` device:
      - Defined `boot.kernelPackages` using `pkgs.linuxPackages`.
      - Enabled kernel modules:
        - `kvm-amd` and `amdgpu` for virtualization and GPU support.
      - Configured `initrd`:
        - Added available kernel modules:
          - `nvme`, `xhci_pci`, `ahci`, `usb_storage`, `usbhid`, `sd_mod`.
        - Loaded `amdgpu` module at boot.

---
+++ system/device/workstation/hardware/kernel/default.nix
@@ -0,0 +1,26 @@
+{ pkgs }:
+
+{
+  boot = {
+    kernelPackages = pkgs.linuxPackages;
+    kernelModules = [
+      "kvm-amd"
+      "amdgpu"
+    ];
+
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ahci"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+      ];
+
+      kernelModules = [
+        "amdgpu"
+      ];
+    };
+  };
+}
