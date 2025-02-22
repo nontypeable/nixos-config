222372f5be2a54cf6beb2876773d1e1b397d61e4
Andrey Sadkov <ad.sadkov@gmail.com>
2025-02-22 15:49:22 +0300

    feat(build-system): updated dependencies and `flake.nix` structure
    - Updated `flake.nix`:
      - Added `flake-parts` to `inputs`.
      - The `outputs` now passes `flake-parts` as an input parameter.
      - Imported `lib` module that accepts `self` and `inputs` for helper functions.
      - Added `supportedSystems` variable containing a list of systems supported by Flake.
    - The `outputs` structure has been rewritten:
      - Used `flake-parts.lib.mkFlake` to create flake.
      - Defined `nixosConfigurations` using the `mkNixosConfig` function from `lib`.
    - Dependencies are committed in the `flake.lock` file.

--- flake.nix
+++ flake.nix
@@ -7,13 +7,28 @@
 
     # Unstable Nixpkgs branch (frequent updates, possible bugs).
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Flake Parts: A tool for managing reusable components in Nix flakes.
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { self, ... }@inputs:
+    { self, flake-parts, ... }@inputs:
     let
       # Hosts description.
       hosts = import ./hosts.nix;
+
+      # Auxiliary code.
+      libx = import ./lib { inherit self inputs; };
+
+      # All flake-supported systems.
+      supportedSystems = libx.allSupportedSystems hosts.devices;
     in
-    { };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = supportedSystems;
+
+      flake = {
+        nixosConfigurations = builtins.mapAttrs libx.mkNixosConfig hosts.devices;
+      };
+    };
 }
