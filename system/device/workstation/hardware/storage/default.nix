cb2b36b649a3af26574cf5f7429692f8c0c6a34c
Andrey Sadkov <ad.sadkov@gmail.com>
2025-02-23 01:28:30 +0300

    fix(workstation-hardware): corrected partition table structure in disk configuration
    - Fixed partition table definition:
      - Added missing `type = "gpt";` to explicitly define the partitioning scheme.
      - Moved `esp`, `root`, and `swap` partitions under the `partitions` attribute.
    - Ensured correct nesting for partitions and subvolumes.

--- system/device/workstation/hardware/storage/default.nix
+++ system/device/workstation/hardware/storage/default.nix
@@ -5,56 +5,59 @@
         type = "disk";
         device = "/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_1TB_S5GXNX1T343557M";
         content = {
-          esp = {
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [ "umask=0077" ];
+          type = "gpt";
+          partitions = {
+            esp = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
             };
-          };
-          root = {
-            size = "900G";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ];
-              subvolumes = {
-                "@" = {
-                  mountpoint = "/";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                    "discard=async"
-                    "ssd"
-                  ];
-                };
-                "@home" = {
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                    "discard=async"
-                    "ssd"
-                  ];
-                  mountpoint = "/home";
-                };
-                "@nix" = {
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                    "discard=async"
-                    "ssd"
-                  ];
-                  mountpoint = "/nix";
+            root = {
+              size = "900G";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                      "ssd"
+                    ];
+                  };
+                  "@home" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                      "ssd"
+                    ];
+                    mountpoint = "/home";
+                  };
+                  "@nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "discard=async"
+                      "ssd"
+                    ];
+                    mountpoint = "/nix";
+                  };
                 };
               };
             };
-          };
-          swap = {
-            size = "8G";
-            content = {
-              type = "swap";
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+              };
             };
           };
         };
