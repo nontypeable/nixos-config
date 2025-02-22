499c875a646d59b019e977cbff556b4f2a56e57f
Andrey Sadkov <ad.sadkov@gmail.com>
2025-02-22 15:12:21 +0300

    feat(config-generator): validate repository before use
    - Updated the `lib` module:
      - Added `assertValidRepository` function to check if the specified repository is valid:
        - If the repository is not in the list of valid repositories (`stable`, `unstable`), an error is thrown.
      - The `mkNixosConfig` now checks the repository before using it.

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,17 @@
 { self, inputs }:
 let
+  allowedRepositories = [
+    "stable"
+    "unstable"
+  ];
+
+  assertValidRepository =
+    repository:
+    if builtins.elem repository allowedRepositories then
+      repository
+    else
+      throw "Error: repository '${repository}' is not allowed. Allowed values: ${builtins.concatStringsSep ", " allowedRepositories}";
+
   mkNixosConfig =
     deviceDirectory:
     {
@@ -7,7 +19,10 @@ let
       repository ? "stable",
       stateVersion,
     }:
-    inputs.${repository}.lib.nixosSystem {
+    let
+      validRepository = assertValidRepository repository;
+    in
+    inputs.${validRepository}.lib.nixosSystem {
       system = platform;
       specialArgs = {
         inherit
