ac8785d34311651a088161e9b5c5e82beb56b4d2
Andrey Sadkov <ad.sadkov@gmail.com>
2025-02-22 14:44:03 +0300

    feat(build-system): added function to get all supported systems
    - Added `getAllSupportedSystems` function:
      - Accepts the `hosts` parameter, an attribute with a description of the hosts.
      - Collects a list of all systems supported by flake, retrieving the `systems.flakeExposed` attributes from the appropriate repositories.

--- lib/default.nix
+++ lib/default.nix
@@ -18,8 +18,32 @@ let
           stateVersion
           ;
       };
+
+      modules = [ ];
     };
+
+  getAllSupportedSystems =
+    hosts:
+    builtins.sort builtins.lessThan (
+      builtins.attrNames (
+        builtins.listToAttrs (
+          map
+            (x: {
+              name = x;
+              value = null;
+            })
+            (
+              builtins.concatLists (
+                map (name: inputs.${hosts.${name}.repository}.lib.systems.flakeExposed or [ ]) (
+                  builtins.attrNames hosts
+                )
+              )
+            )
+        )
+      )
+    );
 in
 {
+  allSupportedSystems = getAllSupportedSystems;
   inherit mkNixosConfig;
 }
