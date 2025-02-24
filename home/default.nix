097dffcf3ccaccae0851ad66780e0b81ce256250
Andrey Sadkov <ad.sadkov@gmail.com>
2025-02-24 22:09:13 +0300

    feat(home-manager): added configuration for the `root` user profile
    - Added support for the `root` profile in `home/default.nix`:
      - Defined `rootConfigDirectory` and `rootPackagesDirectory` for storing superuser configuration and packages.
      - Included the `root` profile in `home-manager.users`, similar to user profiles.
      - Configured imports with directory existence checks.
    - Implemented the configuration module for the `root` user profile:
      - `home/users/root/packages/default.nix` defines system administration packages, grouped by categories:
        - Security & monitoring (`sysstat`, `iotop`, `lm_sensors`, `smartmontools`).
        - Networking tools (`nmap`, `tcpdump`, `mtr`, `whois`, `speedtest-cli`).
        - Hardware diagnostics (`usbutils`, `pciutils`).
      - Enabled the `module.root.packages` module in `home/users/root/default.nix`, responsible for managing root-installed packages.

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,9 @@
 let
   userConfigDirectory = "${self}/home/users/${username}";
   userPackagesDirectory = "${userConfigDirectory}/packages";
+
+  rootConfigDirectory = "${self}/home/users/root";
+  rootPackagesDirectory = "${rootConfigDirectory}/packages";
 in
 {
   home-manager = {
@@ -19,6 +22,15 @@ in
         imports = [
           (if builtins.pathExists userConfigDirectory then userConfigDirectory else null)
           (if builtins.pathExists userPackagesDirectory then userPackagesDirectory else null)
+        ];
+
+        home.stateVersion = stateVersion;
+      };
+
+      root = {
+        imports = [
+          (if builtins.pathExists rootConfigDirectory then rootConfigDirectory else null)
+          (if builtins.pathExists rootPackagesDirectory then rootPackagesDirectory else null)
         ];
 
         home.stateVersion = stateVersion;
